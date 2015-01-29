b0450815ae590d9bf9107b5a55a32fb325f96bc3
visionhackerstudio <visionhackerstudio@gmail.com>
2015-01-29 23:02:52 +0800

    Initial commit

---
+++ src/include/prep.h
@@ -0,0 +1,20 @@
+
+#ifndef __PREP_H__
+#define __PREP_H__
+
+#include "opencv2/opencv.hpp"
+//#include <objbase.h>
+#include <stdlib.h>
+#include <stdio.h>
+//#include <io.h>
+#include <iostream>
+#include <assert.h>
+#include <algorithm>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+using namespace cv;
+
+#endif
+/* endif __PREP_H__ */
